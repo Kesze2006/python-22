5b9502eb2101cb77e2da5ab11dc5843f859e5c9c
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2022-09-12 12:05:15 +0200

    asd

---
+++ feladat1.py
@@ -0,0 +1,2 @@
+print("Hello World", end="\n  \n",)
+print("Hello World", "Hello vilag",42,sep="---")
---
+++ haz.py
@@ -0,0 +1,3 @@
+print ("Keszericze Ákos")
+print ("2022.09.09")
+print ("\t__\n      _ ||\n     / \||\n    /   \|\n   /     \ \n   |     |\n   |     |\n   ------- ")
