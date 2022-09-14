35aa3c33ec6a392ae6bda976c61f1e1c9476f921
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2022-09-14 13:28:51 +0200

    asd

---
+++ HF/szorzotabla.py
@@ -0,0 +1,30 @@
+x=1
+print ("\t1-es")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+print ("\t2-es")
+x=2
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=3
+print ("\t3-mas")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=4
+print ("\t4-es")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=5
+print ("\t5-ös")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=6
+print ("\t6-os")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=7
+print ("\t7-es")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=8
+print ("\t8-as")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=9
+print ("\t9-es")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
+x=10
+print ("\t10-es")
+print (x*1,x*2,x*3,x*4,x*5,x*6,x*7,x*8,x*9,x*10,)
