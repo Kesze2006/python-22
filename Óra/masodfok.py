16f97bb241e9875d28d0967d63313db84c9f8522
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2022-10-28 08:44:53 +0200

    asdf

--- Óra/masodfok.py
+++ Óra/masodfok.py
@@ -1,4 +1,4 @@
-import=match
+import math
 #a*x2+b*x+c
 
 a = int(input("a="))
@@ -7,4 +7,18 @@ c = int(input("c="))
 
 
 #(-b+-gyök(b2-4ac) )/ 2a
+diszkriminans = b*b-4*a*c
+if diszkriminans < 0:
+    print("Nincs megoldás")
+elif diszkriminans == 0: 
+    megoldas = -b / (2*a)
+    print("1 megoldás: {}".format(megoldas))
+else:
+    x1 = (-b+math.sqrt(diszkriminans)) / (2*a)
+    x2 = (-b-math.sqrt(diszkriminans)) / (2*a)
+    print(x1)
+    print(x2)
+
+#gyok = math.sqrt(b*b-4*a*c)
+#print(gyok)
 
