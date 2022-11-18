e16c9c4b17c7a5c7a471eb0198c1c21847c1408f
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 14:04:01 +0100

    asdf

--- Óra/SzamBe.py
+++ Óra/SzamBe.py
@@ -4,10 +4,20 @@
 
 
 def szamBe(szoveg,tort=False,minimum=False,maximum=False):
-    print(szoveg)
-    print(tort) 
+    #print(szoveg)
+    #print(tort) 
+    #rint(minimum)
+    hiba = True
+    while hiba: 
+        try:
+            if tort:
+                szam = float(input(szoveg))
+            else:
+                szam = int(input(szoveg))
+        except:
+            print("Hiba!!")
+        else:
+            hiba = False
 
 
-
-
-szamBe("Aggyá meg számót:  ",tort = True)
+szamBe("Aggyá meg számót:  ",tort = True,minimum= 10)
