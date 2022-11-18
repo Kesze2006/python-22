13d9d76128eb48021f15ed088503fffe3aa4f5dc
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2022-11-18 13:20:15 +0100

    sad

---
+++ Óra/SzamBe.py
@@ -0,0 +1,13 @@
+#Számbekérő modul
+#Többféle paraméterekkel
+#"§"".''.'( łÄđzÄrÍ&zÄ ßłođ
+
+
+def szamBe(szoveg,tort=False,minimum=False,maximum=False):
+    print(szoveg)
+    print(tort) 
+
+
+
+
+szamBe("Aggyá meg számót:  ",tort = True)
