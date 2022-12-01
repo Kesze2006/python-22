209a7d3d849a9d2d9b111bba2d1b1f9c6470ac1e
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2022-12-01 13:19:51 +0100

    sada

--- Óra/gyakorlass 12.py
+++ Óra/gyakorlass 12.py
@@ -1,6 +1,29 @@
 
-lista=[]
-for i in range(10):
+lista=[1,2,3,4,5,6,7,8,9,0]
+for i in range(0):
     lista.append(int(input("Kérek egy számot: ")))
 
-print(lisat)
+print(lista)
+
+for i in range(len(lista)):
+    print(lista[i],end=" ")
+    if i%3==2:
+        print()
+
+print()
+
+szamBe=int(input("Kérek egy számot: "))
+if szamBe in lista:
+    print("Van benne")
+else:
+    print("Nincs benne")
+
+def oszlopba(munkalista,db):
+    for i in range(len(munkalista)):
+        print(munkalista[i],end=" ")
+    if i%db==db-1:
+        print()
+    print()
+
+
+oszlopba(lista,5)
