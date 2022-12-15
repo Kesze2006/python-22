1a9c1f9c1aaebaba5cbc44eed6cbf9440134676b
Kesze2006 <98027625+Kesze2006@users.noreply.github.com>
2022-12-15 21:04:32 +0100

    Add files via upload

---
+++ HF/gyakorlasdogara.py
@@ -0,0 +1,73 @@
+import math
+szam = [12,14,15,23,45,67,89,78]
+#for i in range(8):
+    #szam.append(int(input("Kérek egy számot: ")))
+
+for i in szam:
+    print(int(i),end=" ")
+
+print()
+
+for i in range(8):
+    if i%4==0:
+        print()
+    print(szam[i],end=" ")
+
+
+print()
+
+print(sum(szam))
+
+
+szoveg = "Nulla quis mi augue. Nunc vel pretium lectus. Aenean laoreet ornare ornare. Ut vitae elit et sapien fringilla iaculis ac at felis. Aenean scelerisque, diam non pellentesque rhoncus, risus lorem porttitor leo, ac consectetur nisi massa vitae sem. Nulla tempus diam id bibendum lobortis. Vestibulum porta neque id risus cursus, eget sodales nunc fermentum. Nulla facilisi. Suspendisse egestas orci a luctus fringilla. Cras dapibus ipsum nisl, non dapibus ex fermentum vitae."
+
+betu = input("Adj meg egy betüt: ")
+
+
+
+
+alma=[]
+for e in szoveg:
+    if betu==e:
+        alma.append(1)
+    else:
+        pass
+
+print(sum(alma))
+
+
+
+print(szoveg[::-1])
+
+
+print(len(szoveg))
+
+szoveg=szoveg.replace("orna","")
+
+print(len(szoveg))
+
+
+l=45
+
+while l != 1:
+    l = int(input("Kérek egy számot: "))
+
+fa="""
+   *
+  ***
+ *****
+*******
+  ***
+ *****
+*******
+  ***
+ *****
+*******
+   *
+  ***   """
+
+fa=fa.replace("*",str(l))
+print(fa)
+
+
+
