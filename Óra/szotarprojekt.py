5eadf4a1f1fa4d505b752da7cc0ed5bd109884bf
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-01-23 08:44:43 +0100

    asd

--- Óra/szotarprojekt.py
+++ Óra/szotarprojekt.py
@@ -35,10 +35,25 @@ def beolvas():
     
     f.close()
 def kerdez():
-    print(kerdesek)
-
-
+    random.seed(2)
+    valasztott=random.choice(kerdesek)
+    print("valasztott:",valasztott)
+    rossz=[]
+    for i in range(3):
+        temp=random.choice(kerdesek)
+        print("temp",temp)
+        while temp not in rossz and temp != valasztott:
+            rossz.append(temp)
+        print("rossz", rossz)   
+    print("-"*40)
+    print("Mit jelent: " + valasztott[0] + "?")
+    
+    rossz.append(valasztott)
+    print(rossz)
+    
+beolvas()
 kerdez()
-beolvas() 
 #szavak=sokbeker()
 #filebair(szavak)
+
+ 
