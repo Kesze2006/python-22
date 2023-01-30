f1966cfd804d58107ef79feb627609d13c58132b
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-01-30 12:28:24 +0100

    asd

--- Óra/szotarprojekt.py
+++ Óra/szotarprojekt.py
@@ -1,30 +1,32 @@
 import random
 
 def szobeker():
-    szo = input("Kérek egy szót: ")
-    if szo == "":
+    szo=input("Kérek egy szót: ")
+    if szo=="":
         jelentes=""
     else:
-        jelentes=input("Mit jelenet az hogy "+szo+"?")
+        jelentes=input(szo+ " jelentése: ")
     return [szo,jelentes]
-
 
 szavak=[]
 def sokbeker():
     szo=szobeker()
-    while szo[0] != "":
+    while szo[0]!="":
         szavak.append(szo)
         szo=szobeker()
-    return(szavak)
 
+
+    return(szavak)
+#print(sokbeker())
 
 def filebair(lista):
     f=open("szotar.txt","a")
+
     for e in lista:
+        #print(e)
         f.write(" ".join(e))
         f.write("\n")
-        #print(e)
-        
+
     f.close()
 
 kerdesek=[]
@@ -32,12 +34,12 @@ def beolvas():
     f=open("szotar.txt","r")
     for sor in f:
         kerdesek.append(sor.replace("\n","").split(" "))
-    
     f.close()
+
 def kerdez():
     #random.seed(2)
     valasztott=random.choice(kerdesek)
-    #print("valasztott:",valasztott)
+    #print("valasztott",valasztott)
     rossz=[]
     for i in range(3):
         temp=random.choice(kerdesek)
@@ -46,48 +48,71 @@ def kerdez():
             temp=random.choice(kerdesek)
             
         rossz.append(temp)
-        #print("rossz", rossz)   
-    print("-"*80)
-    print("Mit jelent: " + valasztott[0] + "?")
-    
+        #print("rossz",rossz)    
+
+    print("-"*40)
+    print("Mit jelent: "+ valasztott[0]+"?")
+
     rossz.append(valasztott)
-    #print(rossz)
-
-
-    abc="abcdefghijklmnopqrstuvxyz"
+    
+    #valasz bekeres
+    abc="abcdefghijklmnopqrstuvz"
     random.shuffle(rossz)
 
     i=0
     for e in rossz:
         print(abc[i]+": "+e[1])
         i+=1
+    valasz=input("Válassz: ")          
 
-    valasz=input("Válassz: ")
     hol=4
-    
-    while hol >= 4:
+    while hol>=4:
         try:
             if valasz!="":
                 hol=abc.index(valasz)
-            else:
-                pass
-            
         except:
-            valasz=input("Válassz újra: ")
+            valasz=input("valassz ujra : ")
         else:
-            if hol >= 4:
-                valasz=input("Válassz újra: ")
-    #print(valasztott)
-    #print(rossz[hol])
-    if valasztott[0]==rossz[hol][0]:
-        print("Jó válasz!")
-    else:
-        print("Nem jó!")
-beolvas()
-for i in range(10):
-    kerdez()
+            if hol>=4:
+                valasz=input("valassz ujra : ")
+            
+    #if valasztott[0]==rossz[hol][0]:
+        #print("helyes :)")
+    #else:
+        #print("rossz a válasz :(")
 
-#szavak=sokbeker()
-#filebair(szavak)
+    return valasztott[0]==rossz[hol][0]
 
- 
+def menu():
+    beker=""
+
+        
+    while beker!="0":
+        print("-"*40)
+        print("szótár program\n")
+        print("1: Szavak bevitele")
+        print("2: Feleltetes")
+        print("0: Kilepes")
+        beker=input("Válasz: ")
+
+        if beker=='1':
+            #adatbekeres
+            szavak=sokbeker
+            filebair(szavak)
+            
+        elif beker=="2":
+            #feleltetés   
+            beolvas()
+            lil_A=[]
+            for i in range(10):
+                lil_A.append(kerdez())
+            #print(lil_A)
+            print("Az eredmeny: {:.0%}".format(lil_A.count(True)/len(lil_A)))   
+menu()
+    
+
+
+
+
+    
+
