e65f3201d819a208678313ca96292181064d485d
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-03-13 13:18:02 +0100

    asd

--- Óra/gyakorlas0307.py
+++ Óra/gyakorlas0307.py
@@ -1,3 +1,16 @@
+def oszlopvissza(hanyadik):
+    oszlop=[]
+    for e in tabla:
+        oszlop.append(e[hanyadik-1])
+    return oszlop
+    
+def alma(hanyasaval):
+    oszlop=[e[hanyasaval-1::hanyasaval] for e in tabla]
+    return oszlop
+
+
+
+
 gyumolcsok=["alma","körte","szőlő","barac","dragonfruit","licsi",]
 
 print("Ennyi gyümölcs van: " + str(len(gyumolcsok)))
@@ -65,6 +78,34 @@ print(gyumolcsok[::-1])
 masolat=gyumolcsok
 masolat.reverse()
 print(masolat)
+
+tabla=[]
+for i in range(20):
+    sor=[]
+    for k in range(10):
+        sor.append((i+1)*(k+1))
+    tabla.append(sor)
+print(tabla)
+
+oszlop=[]
+for e in tabla:
+    #print(e)
+    oszlop.append(e[0])
+print(oszlop)    
+print(oszlopvissza(5))
+print(oszlopvissza(10))
+print()
+oszlop=[e[:3] for e in tabla]
+oszlop=[e[4:7] for e in tabla]
+oszlop=[e[3::4] for e in tabla]
+print(oszlop)
+print()
+#print(alma (int(input("Kérek egy számot: "))))ű
+
+oszlop=[[e[2],e[6],e[0]] for e in tabla]
+print(oszlop)
+
+
 
 
 
