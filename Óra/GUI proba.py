a93a331d90e1220dfcf6e464edcc11295e3e33be
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-04-14 08:57:43 +0200

    asd

--- Óra/GUI proba.py
+++ Óra/GUI proba.py
@@ -1,17 +1,64 @@
 from tkinter import *
+
+def eltol(pontok,x,y):
+    vissza=[]
+    for i, pont in enumerate(pontok):
+        if i%2==1:
+            vissza.append(pont+y)
+        else:
+            vissza.append(pont+x)
+    return vissza
+
+def nagyit(pontok,meret=1):
+    vissza=[]
+    for i, pont in pontok:
+        vissza.append(pont*meret)
+    return vissza
+
+
+    
+
+
+
+
+
+
+
+
+
+
 win=Tk()
 
 # Set the size of the tkinter window
-win.geometry("700x500+500+500")
+win.geometry("700x500")
 
 # Create a canvas widget
-canvas=Canvas(win, width=500, height=300, bg="green")
+canvas=Canvas(win, width=500, height=300, bg="skyblue")
 canvas.pack(fill = BOTH, expand = 1)
 
-#canvas2=Canvas(win, width=500, height=300, bg="skyblue")
-#canvas2.pack(fill = BOTH, expand = 1)
-# Add a line in canvas widget
-canvas.create_line(100,100,300,100,300,300,100,300,100,100, fill="red", width=10)
-canvas.create_line(300,300,150,300,300,300, fill="blue", width=10)
+
+M = [100,100,300,100,300,300,100,300,100,100]
+
+
+canvas.create_line(M, fill="red", width=5)
+
+m2=eltol(M,100,-100)
+
+canvas.create_line(m2, fill="purple", width=5)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 win.mainloop()
