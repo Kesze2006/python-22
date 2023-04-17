31634adb580613a3ad1075f32b9f0da3831c1bb7
Keszericze Ákos <keszericzea21@ADY-NAGYATAD.SULINET.HU>
2023-04-17 08:44:12 +0200

    sad

--- HF/teljesnev.py
+++ HF/teljesnev.py
@@ -1,5 +1,6 @@
-from tkinter import *
+import math
 
+from tkinter import *
 
 def eltol(pontok,x,y):
     vissza=[]
@@ -17,11 +18,19 @@ def nagyit(pontok,meret=1):
     return vissza
 
 
-def forgat(pontok, szog):
+def forgat(pontok,szog):
     vissza=[]
     for i, pont in enumerate(pontok):
         if i%2 == 0:
-            x=pontok[i]*math.cos(math.radians(szog)) - pontok[i+1]*math
+            
+            
+            x=pontok[i]*math.cos(math.radians(szog))-pontok[i+1]*math.sin(math.radians(szog))
+            
+            y=pontok[i]*math.sin(math.radians(szog))+pontok[i+1]*math.cos(math.radians(szog))
+            
+            vissza.append(x)
+            vissza.append(y)
+        return vissza        
 
 
 
@@ -35,43 +44,38 @@ canvas.pack(fill = BOTH, expand =1)
 
 
 
-#canvas.create_line(250,0,250,500, fill="blue", width=5)
-#canvas.create_line(500,0,500,500, fill="blue", width=5)
-#canvas.create_line(750,0,750,500, fill="blue", width=5)
+AKOS=[[0,375,125,125,250,375,175,375,150,300,100,300,75,375,0,375,],
+      [100,250,125,200,150,250,100,250,],
+      [175,150,175,100,200,100,200,150,175,150,],
+      [250,375,250,125,333,125,333,250,416,125,500,125,417,250,500,375,416,375,360,300,333,375,250,375,],
+      [500,125,750,125,750,375,500,375,500,125,],
+      [550,175,700,175,700,325,550,325,550,175,],
+      [1000,125,750,125,750,249,936,249,936,311,750,311,750,375,1000,375,1000,210,812,210,810,160,1000,160,1000,125,]]
+
+kozep=[0,0]
+db=0
+for betu in AKOS:
+    xK=betu[::2]
+    yK=betu[1::2]
+    kozep[0]+=sum(xK)
+    kozep[1]+=sum(yK)
+    db+=len(xK)
+    
+kozep[0]/=db
+kozep[1]/=db
 
 
-"""#Á
-canvas.create_line(0,375,125,125,250,375,175,375,150,300,100,300,75,375,0,375, fill="red", width=10)
-canvas.create_line(100,250,125,200,150,250,100,250,fill="orange", width=10)
-canvas.create_line(175,150,175,100,200,100,200,150,175,150,fill="yellow", width=10)
 
-#K
-canvas.create_line(250,375,250,125,333,125,333,250,416,125,500,125,417,250,500,375,416,375,360,300,333,375,250,375,fill="green", width=10)
 
-#O
-canvas.create_line(500,125,750,125,750,375,500,375,500,125,fill="blue", width=10)
-canvas.create_line(550,175,700,175,700,325,550,325,550,175,fill="indigo", width=10)
+for betu in AKOS:
+    betu=eltol(betu,kozep[0],kozep[1])
+    betu=forgat(betu,10)
+    canvas.create_line(betu,fill="black",width=10)
 
-#S
-canvas.create_line(1000,125,750,125,750,249,936,249,936,311,750,311,750,375,1000,375,1000,210,812,210,810,160,1000,160,1000,125,fill="violet", width=10)
-"""
-A=[0,375,125,125,250,375,175,375,150,300,100,300,75,375,0,375,]
-kisA=[100,250,125,200,150,250,100,250,]
-vesszo=[175,150,175,100,200,100,200,150,175,150,]
-K=[250,375,250,125,333,125,333,250,416,125,500,125,417,250,500,375,416,375,360,300,333,375,250,375,]
-O=[500,125,750,125,750,375,500,375,500,125,]
-kisO=[550,175,700,175,700,325,550,325,550,175,]
-S=[1000,125,750,125,750,249,936,249,936,311,750,311,750,375,1000,375,1000,210,812,210,810,160,1000,160,1000,125,]
 
-K=eltol(K,100,0)
-K=nagyit(K,2)
-#canvas.create_line(A,fill="red",width=10)
-#canvas.create_line(kisA,fill="orange",width=10)
-#canvas.create_line(vesszo,fill="yellow",width=10)
-canvas.create_line(K,fill="green",width=10)
-#canvas.create_line(O,fill="blue",width=10)
-#canvas.create_line(kisO,fill="indigo",width=10)
-#canvas.create_line(S,fill="violet",width=10)
+
+
+
 
 
 
