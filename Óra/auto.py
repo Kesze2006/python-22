8940345575db2edb2b259417d1ef55042b999d24
Kesze2006 <keszericze.akos.21@ady-nagyatad.hu>
2023-04-25 20:56:44 +0200

    asd

--- Óra/auto.py
+++ Óra/auto.py
@@ -1,3 +1,4 @@
+import random
 class auto:
     szin=""
     ajtokszama=0
@@ -15,13 +16,82 @@ class auto:
     def index(self):
         print("Kat-Kat!")
 
+    def __str__(self):
+        return self.marka
+    
+
 if __name__ == '__main__':
-    q=auto("piros",5,"BMW","e34")
+    q=auto("piros",5,"Nissan","GTR")
+    print(q)
     q.inditas()
     q.duda()
     q.index()
-
+    print()
 
 class bmw(auto):
-    def __init__(self)
-    super().inditas
+    def __init__(self,szin,ajtokszama,marka,tipus):
+        super().__init__(szin,ajtokszama,marka,tipus)
+
+    def inditas(self):
+        print("Brum!!")
+    def index(self):
+        print("*Tücsökciripelés*")
+
+    def __str__(self):
+        return self.marka
+
+
+if __name__ == '__main__':
+    bmw=bmw("fekete",5,"BMW","e34")
+    print(bmw)
+    bmw.inditas()
+    bmw.index()
+    bmw.duda()
+    print()
+
+
+class merci(auto):
+    def __init__(self,szin,ajtokszama,marka,tipus):
+        super().__init__(szin,ajtokszama,marka,tipus)
+
+    def duda(self):
+        print("Du-Du!")
+        
+    def __str__(self):
+        return self.marka
+
+if __name__ == '__main__':
+    merci=merci("lila",3,"Mercédesz","B-osztály")
+    print(merci)
+    merci.inditas()
+    merci.index()
+    merci.duda()
+    print()
+
+class audi(auto):
+    def __init__(self,szin,ajtokszama,marka,tipus):
+        super().__init__(szin,ajtokszama,marka,tipus)
+    a=random.randint(1,2)
+    def checkengine(self):
+        pass
+    if a == 1:
+        engine=True
+    else:
+        engine=False
+        
+    if engine == True:
+        def checkengine(self):
+            print("Ég a check engine!!")
+
+
+if __name__ == '__main__':
+    audi=audi("sárga",5,"Audi","RS6")
+    print(audi)
+    audi.inditas()
+    audi.index()
+    audi.duda()
+    audi.checkengine()
+    print()
+
+
+    
